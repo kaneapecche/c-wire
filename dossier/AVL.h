1da086a2315bfedc6be79b3a3d66299383c31738
mejdo5 <mejdoline_ketata@yahoo.com>
2024-12-16 11:12:06 +0100

    Update AVL.h

--- dossier/AVL.h
+++ dossier/AVL.h
@@ -1,3 +1,6 @@
+#ifndef AVL_H
+#include "verification.h"
+
 typedef struct arbre{
     int id_station;
     struct arbre* gauche;
@@ -6,3 +9,34 @@ typedef struct arbre{
     int capacite_total;
     int conso_total;
 }Arbre;
+
+Arbre* creation(int s);
+
+int estVide(Arbre* racine);
+
+int existeGauche(Arbre* racine);
+
+int existeDroit(Arbre* racine);
+
+int estFeuille(Arbre* racine);
+
+Arbre* rotationgauche(Arbre* a);
+
+Arbre* rotationdroite(Arbre* a);
+
+int hauteur(Arbre* racine);
+
+int facteur(Arbre* racine);
+
+int doublerotationdroite(Arbre* a);
+
+int doublerotationgauche(Arbre* a);
+
+Arbre* equilibrage(Arbre *a);
+
+Arbre*insertionAVL (Arbre* a, int id_station ,int *h, int capa, int conso);
+
+#endif
+
+Arbre* recherche( Arbre* a, int s);
+
