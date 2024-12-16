3b7607a1d06838ee0c2d97107d85466464d1aace
mejdo5 <mejdoline_ketata@yahoo.com>
2024-12-16 10:42:28 +0100

    Update verification.h

--- verification.h
+++ verification.h
@@ -1,1 +1,17 @@
+#ifndef VERIFICATION_H
+#define VERIFICATION_H
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<AVL.h>
+#include<string.h>
+
+void verificationalloc();
+
+void verificationferme(FILE* fichier);
+
+void erreurs(int argc, char *argv[]);
+
+void verification(Arbre * a);
+
+#endif
