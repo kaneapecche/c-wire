61d5f8d94d0ffd0bda9fd6130ffcf6fbc2b61cfa
mejdo5 <mejdoline_ketata@yahoo.com>
2024-12-16 11:51:04 +0100

    Update main.c

--- dossier/main.c
+++ dossier/main.c
@@ -9,15 +9,18 @@
 
 int main(){
     Arbre* racine;
-    File* f = fopen("sortie.txt", "w");
+    FILE* f = fopen("sortie.txt", "w");
     if(f==NULL){
         perror("Erreur lors de l'ouverture du fichier");
         return 1;
     }
-    int id, capa, conso
+    int id, capa, conso;
     while(scanf("%d %d %d\n", &id, &capa, &conso)==3){
-        racine=insertionAVL(racine, id, *h, capa, conso);//la somme est calculée dans la fonction insertionAVL
-        fprintf(f, "%d %d %d\n", id, capa, conso);//voir s'il est possible de fzaire la somme directment dans la fonction insertion
+    	int h= hauteur(racine);
+        racine=insertionAVL(racine, id, &h, capa, conso);
+        //la somme est calculée dans la fonction insertionAVL
+        fprintf(f, "%d %d %d\n", id, capa, conso);
+        //voir s'il est possible de fzaire la somme directment dans la fonction insertion
     }
     fclose(f);
     return 0;
